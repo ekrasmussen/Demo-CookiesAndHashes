04bfd9bedf7881c34b1e7ebdbab6d622cd5e8c14
Emil Rasmussen <emil@erasmussen.dk>
2022-05-03 15:44:02 +0200

    Added Login and logout methods in controller

---
+++ CookiesAndHashes-ClassLibrary/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CookiesAndHashes_ClassLibrary
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+        public string Role { get; set; }
+    }
+}
--- CookiesAndHashes/Controllers/HomeController.cs
+++ CookiesAndHashes/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using CookiesAndHashes.Models;
+using CookiesAndHashes_ClassLibrary;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace CookiesAndHashes.Controllers
 {
@@ -18,6 +22,84 @@ namespace CookiesAndHashes.Controllers
             return View();
         }
 
+        /// <summary>
+        /// When we just make a GET request, we want to just show the login form
+        /// </summary>
+        /// <returns>The Login View</returns>
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //When we make a post request from the Login View, we want to try authentication with the info from
+        //The username and password text field!
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            //Check if either textbox is empty
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ViewBag.Message = "Username or Password cannot be empty";
+                return View();
+            }
+
+            //THIS IS THE AUTHENTICATION PART
+            //Replace this with your means of authenticating
+            //In our case, i have a API with a mock database that has username and hashed password stored, along with role
+            //From here, please look at AuthenticateUser to see how Hashing and Salting works
+            User foundUser = AuthenticateUser(username, password);
+
+            //If we found a user, meaning the user successfully typed in the right username and password, we make a cookie
+            //Otherwise we return to the same page with a new ViewBag Message
+            if(foundUser == null)
+            {
+                ViewBag.Message = "Wrong username or password";
+                return View();
+            }
+
+            //This is the cookie creation, we use claims to insert specific info into the cookie, like role, id etc
+            var claims = new List<Claim>
+            {
+                //We can add more claims to the list, lets add the username, and their role.
+                //Claimtypes has default values for name and Role. But you can add whatever you want using strings (its just a Dictionary)
+                new Claim(ClaimTypes.Name, foundUser.Username),
+                new Claim(ClaimTypes.Role, foundUser.Role)
+                //new Claim("AnythingYouWant", "AnyValue");
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            //This is optional configuration
+            var authProperties = new AuthenticationProperties
+            {
+                AllowRefresh = true,
+
+                //Cookie stays after closing browser
+                IsPersistent = true,
+
+                //How long the cookie is valid
+                ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
+            };
+
+            //finally, sign in time!
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties
+                );
+
+            return RedirectToAction("Index");
+        }
+
+        //And of course we want to be able to log out again
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -27,6 +109,17 @@ namespace CookiesAndHashes.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public User AuthenticateUser(string username, string password)
+        {
+            Login login = new Login
+            {
+                Username = username,
+                PlaintextPassword = password
+            };
+
+            return null;
         }
     }
 }
