8d98f69adfd5f8cb6dba1a9b56b0937c4a55829e
Emil Rasmussen <emil@erasmussen.dk>
2022-05-03 16:38:28 +0200

    Added API and created a little console app for generating hashes

---
+++ CookiesAndHashes-API/Database/MockDatabase.cs
@@ -0,0 +1,21 @@
+﻿using CookiesAndHashes_ClassLibrary;
+
+namespace CookiesAndHashes_API.Database
+{
+    public class MockDatabase
+    {
+        public List<User> users;
+
+        private MockDatabase()
+        {
+            users = new List<User>
+            {
+                new User()
+                {
+                    Username = "Breiner",
+                    HashedPassword = 
+                }
+            };
+        }
+    }
+}
---
+++ HashGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CookiesAndHashes.Services;
+
+string password = Console.ReadLine()!;
+
+string salt = Console.ReadLine()!;
+
+string hashed = HashAndSaltGenerator.GetHash(password, salt);
+
+Console.WriteLine($"PasswordHash: {hashed}");
+Console.WriteLine($"Salt: {salt}");
