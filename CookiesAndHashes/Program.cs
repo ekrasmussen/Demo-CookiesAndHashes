8c70d264e0a3e907bdbf08760542088932722d4d
Emil Rasmussen <emil@erasmussen.dk>
2022-05-03 14:59:33 +0200

    Added Cookie Middleware to Program.cs

--- CookiesAndHashes/Program.cs
+++ CookiesAndHashes/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+/// <summary>
+/// Add Authentication to the services, in the parameters we explain the authentication scheme we will use.
+/// In this case we also use .AddCookie();, but you could also do something like .AddOAuth() if we wanted to use OAuth instead!
+/// </summary>
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
 var app = builder.Build();
 
@@ -18,7 +27,18 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//..And then we also make sure that we add UseAuthentication to our middleware after adding the cookie Authentication
+//KEEP IN MIND, The ordering of these methods are very sensitive, make sure you put them in the right places in relation to the default methods!
+app.UseAuthentication();
 app.UseAuthorization();
+
+CookiePolicyOptions cpo = new CookiePolicyOptions
+{
+    //This defines the policy on how the cookie is used, SameSiteMode specifies the Cookie constraints
+    //See https://docs.microsoft.com/en-us/dotnet/api/system.web.samesitemode?view=netframework-4.8
+
+    MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Lax
+};
 
 app.MapControllerRoute(
     name: "default",
